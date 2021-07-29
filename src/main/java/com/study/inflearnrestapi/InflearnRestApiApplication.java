c727dccde4f87a417a6518ecfd7cbdedd950ee63
tychejin1218 <tychejin@naver.com>
2021-07-29 16:22:23 +0900

    Initial Commit

---
+++ src/main/java/com/study/inflearnrestapi/InflearnRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.inflearnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/inflearnrestapi/InflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.inflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
