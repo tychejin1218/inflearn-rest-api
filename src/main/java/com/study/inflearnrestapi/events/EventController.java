6be2dd396738e7fd856473d63b2b9fb937c22fe3
tychejin1218 <tychejin@naver.com>
2021-08-04 17:25:16 +0900

    스프링 HATEOAS 적용

--- src/main/java/com/study/inflearnrestapi/events/EventController.java
+++ src/main/java/com/study/inflearnrestapi/events/EventController.java
@@ -1,7 +1,9 @@
 package com.study.inflearnrestapi.events;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -40,9 +42,18 @@ public class EventController {
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
+        Integer id = event.getId();
         event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createUri = linkTo(EventController.class).slash("{id}").toUri();
-        return ResponseEntity.created(createUri).body(event);
+
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(id);
+        URI createdUri = selfLinkBuilder.toUri();
+
+        EntityModel entityModel = EntityModel.of(event);
+        entityModel.add(linkTo(EventController.class).slash(id).withSelfRel());
+        entityModel.add(linkTo(EventController.class).withRel("query-events"));
+        entityModel.add(selfLinkBuilder.withRel("update-event"));
+
+        return ResponseEntity.created(createdUri).body(entityModel);
     }
 }
