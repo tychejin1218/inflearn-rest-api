b8df5f3e4e162f53597cf3b4bcf08969cd76e9f5
jypark <jypark@eduhansol.co.kr>
2021-08-05 13:59:37 +0900

    스프링 REST Docs 문서 빌드

--- src/main/java/com/study/inflearnrestapi/events/EventController.java
+++ src/main/java/com/study/inflearnrestapi/events/EventController.java
@@ -2,6 +2,7 @@ package com.study.inflearnrestapi.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -53,6 +54,7 @@ public class EventController {
         entityModel.add(linkTo(EventController.class).slash(id).withSelfRel());
         entityModel.add(linkTo(EventController.class).withRel("query-events"));
         entityModel.add(selfLinkBuilder.withRel("update-event"));
+        entityModel.add(Link.of("docs/index.html#resources-events-create").withRel("profile"));
 
         return ResponseEntity.created(createdUri).body(entityModel);
     }
