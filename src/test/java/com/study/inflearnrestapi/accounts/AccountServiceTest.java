6d7ba6807018b547af2de8602299f2ec1a9e8f3b
tychejin1218 <tychejin@naver.com>
2021-08-11 10:45:22 +0900

    스프링 시큐리티 적용

--- src/test/java/com/study/inflearnrestapi/accounts/AccountServiceTest.java
+++ src/test/java/com/study/inflearnrestapi/accounts/AccountServiceTest.java
@@ -35,6 +35,7 @@ public class AccountServiceTest {
                 .password(password)
                 .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
                 .build();
+
         this.accountRepository.save(account);
 
         // When
