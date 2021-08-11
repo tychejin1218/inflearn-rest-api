4c9a1885a9528d539457bde2f9773841dec47528
tychejin1218 <tychejin@naver.com>
2021-08-11 13:44:07 +0900

    스프링 시큐리티 폼 인증 설정

--- src/main/java/com/study/inflearnrestapi/config/AppConfig.java
+++ src/main/java/com/study/inflearnrestapi/config/AppConfig.java
@@ -1,10 +1,18 @@
 package com.study.inflearnrestapi.config;
 
+import com.study.inflearnrestapi.accounts.Account;
+import com.study.inflearnrestapi.accounts.AccountRole;
+import com.study.inflearnrestapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +25,24 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account admin = Account.builder()
+                        .email("admin@email.com")
+                        .password("password01")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(admin);
+            }
+        };
     }
 }
--- src/main/java/com/study/inflearnrestapi/config/SecurityConfig.java
+++ src/main/java/com/study/inflearnrestapi/config/SecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -53,4 +55,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/docs/index.html").anonymous()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).anonymous();
     }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .anonymous()
+            .and()
+                .formLogin()
+            .and()
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/api/**").anonymous()
+                .anyRequest().authenticated();
+    }
 }
